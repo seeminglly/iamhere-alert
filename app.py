be88cb838c5d2a1d21d27e996eda295366cd84cf
seeminglly <seeminglly@naver.com>
2025-04-03 23:37:38 +0900

    정시 분에 알람 전송

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
 from flask import Flask
 from scheduler import start_scheduler
+#from scheduler import check_attendance
+import asyncio
+from quart import Quart
 
-app = Flask(__name__)
+#Flask 객체 인스턴스 생성
+app = Quart(__name__)
 
 @app.route('/')
 def index():
     return "출석 알람 시스템 작동 중입니다!"
 
-if __name__ == '__main__':
-    start_scheduler()
-    app.run(debug=True)
+async def main():
+    start_scheduler()  # AsyncIOScheduler 등록
+    await app.run_task(debug=True)  # 비동기 서버 실행
+
+if __name__ == "__main__":
+    asyncio.run(main())
--- scheduler.py
+++ scheduler.py
@@ -1,28 +1,62 @@
-from apscheduler.schedulers.background import BackgroundScheduler
+#from apscheduler.schedulers.background import BackgroundScheduler
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from datetime import datetime, timedelta
+import asyncio
+from datetime import datetime
+
+#name : 사용자 이름
+#schedule_time[] : 사용자의 스케쥴 시작 시간(ex.09:00)
+
 
 users = [
-    {"name":"김수정", "schedule_time":"09:00"},
-    {"name":"수룡이", "schedule_time":"12:00"},
-    {"name":"강세민", "schedule_time":"11:39"},
-    {"name":"홍길동", "schedule_time":"11:42"},
+    {
+    "name":"강세민",
+    "schedule_time":["23:40","23:41","23:42"]
+    },
+     {
+    "name":"백재승",
+    "schedule_time":["23:40","23:41","23:43"]
+    },
+     {
+    "name":"홍길동동",
+    "schedule_time":["23:40","23:41","23:44"]
+    }
 ]
 
-def check_attendance():
+
+async def send_alert(name, time):
     now = datetime.now()
+    print(f"🔔 {name}님! 출석 시작 10분 전입니다. ({time})")
+    print("현재 시간 : ", now.time())
+    await asyncio.sleep(0.01)
+
+async def check_attendance(): #APScheduler가 실행하는 함수
+    now = datetime.now().replace(second=0, microsecond=0)
+    print(f"[{now.strftime('%H:%M')}] 출석 알림 점검 중...")
+   
+
+
+    tasks = [] # 비동기 작업 리스트
 
     for user in users:
+        for time in user["schedule_time"]:
         # 문자열을 datetime으로 변환
-        user_time = datetime.strptime(user["schedule_time"], "%H:%M")
-        # 오늘 날짜 + 출석 시간으로 통일
-        att_time = now.replace(hour=user_time.hour, minute=user_time.minute, second=0)
+            user_time = datetime.strptime(time, "%H:%M") #datetime객체로 변환
+            # 오늘 날짜 + 출석 시간으로 통일
+            att_time = now.replace(hour=user_time.hour, minute=user_time.minute, second=0)
+            alarm_start = att_time - timedelta(minutes=10, seconds=10)
+            alarm_end = att_time - timedelta(minutes=9, seconds=50)
+
+            if alarm_start <= now <= alarm_end:
+                # 알림을 비동기 task로 추가
+                tasks.append(send_alert(user["name"], time))
+     # 등록된 모든 알림 작업을 동시에 실행
+    await asyncio.gather(*tasks)
+
         
-        # 출석 10분 전인지 확인
-        if now >= att_time - timedelta(minutes=10) and now < att_time - timedelta(minutes=9):
-            print(f"🔔 {user['name']}님! 출석 시간 10분 전입니다. ({user['schedule_time']})")
-
-
+#asyncio.run(check_attendance())
 def start_scheduler():
-    scheduler = BackgroundScheduler()
-    scheduler.add_job(check_attendance, 'interval', minutes=1)  # 매 1분마다 실행
+    scheduler = AsyncIOScheduler()
+    scheduler.add_job(check_attendance, 'cron', second=0)
     scheduler.start()
+
